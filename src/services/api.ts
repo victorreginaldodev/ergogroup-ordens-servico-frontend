6f85d302178ddbbe58ed42fd856eb22c2431b9a2
Dev Victor <contato.victordev02@gmail.com>
2026-06-29 13:15:04 -0300

    implementa variáveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// URL base da API - altere aqui para desenvolvimento
-// const API_BASE_URL = 'https://www.ergogroupapp.com/';
-const API_BASE_URL = 'http://127.0.0.1:8000/'; // URL para desenvolvimento local
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
